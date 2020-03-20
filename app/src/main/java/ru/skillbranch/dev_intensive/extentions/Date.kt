5daaf58d7b50071436c9be333657035fcd9002e9
Rustem.Garaev <Tel52093547!@>
2020-03-20 14:18:19 +0300

    hometask_2 version #2

---
+++ app/src/main/java/ru/skillbranch/dev_intensive/extentions/Date.kt
@@ -0,0 +1,40 @@
+package ru.skillbranch.dev_intensive.extentions
+
+import java.util.*
+
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+
+fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
+    val dateFormate = java.text.SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormate.format(this)
+}
+
+var diffValue = 0
+var difUnits: TimeUnits = TimeUnits.SECOND
+
+fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
+    var time = this.time
+
+    time += when (units) {
+        TimeUnits.SECOND -> value * SECOND
+        TimeUnits.MINUTE -> value * MINUTE
+        TimeUnits.HOUR -> value * HOUR
+        TimeUnits.DAY -> value * DAY
+    }
+    this.time = time
+
+    diffValue = value
+    difUnits = units
+
+    return this
+}
+
+enum class TimeUnits {
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY
+}
